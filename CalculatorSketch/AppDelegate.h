eb10563ffe8ec44c68afebb980bfcd5d60f22ccf
enderqiu <a934560824@163.com>
2018-06-07 15:16:32 +0800

    Initial Commit

---
+++ CalculatorSketch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculatorSketch
+//
+//  Created by enderqiu on 2018/6/7.
+//  Copyright © 2018年 enderqiu.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
